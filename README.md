8165646116e2ae3ec6e4fac38dbc1711f11038bb
Ville Ranki <ville.ranki@iki.fi>
2019-06-19 11:48:28 +0300

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pam-auth-rest-api
-REST api for authenticating via PAM - for use with Matrix mxisd
+
+REST api for authenticating via PAM - for use with Matrix mxisd rest authenticator.
+
+WIP / Alpha
+
+## Installation:
+
+```npm install```
+
+On ubuntu/debian building authenticate-pam requires dev package ```libpam0g-dev```
+
+## Running
+
+```node index.js```
+
+For custom port, use env variable:
+
+```PORT=4242 node index.js```
+
+### Curl test command
+
+```curl -X POST -d "{ \"auth\": { \"mxid\": \"@john.doe:example.org\", \"localpart\": \"john.doe\", \"domain\": \"example.org\", \"password\": \"passwordOfTheUser\" } }" -H "Content-Type: application/json" http://localhost:3000/_mxisd/backend/api/v1/auth/login```
---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require('body-parser')
+var pam = require('authenticate-pam');
+var app = express();
+app.use( bodyParser.json() );
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+app.post("/_mxisd/backend/api/v1/auth/login", (req, res, next) => {
+    var username = req.body.auth.localpart
+    var password = req.body.auth.password
+
+    pam.authenticate(username, password, function(err) {
+        var resobject = { 
+            "auth": {
+                "success": false
+            }
+        }
+        if(err) {
+          console.log("PAM authentication failed: ", err);
+        } else {
+          resobject["auth"]["success"] = true
+          resobject["auth"]["id"] = {
+            "type": "localpart",
+            "value": username
+          }
+        }
+        res.json(resobject);
+    });
+});
+
+app.listen(port, () => {
+ console.log("pam-auth-rest-api server running on port", port);
+});
